01535ad8a3330555d2cd1901c4bd7714eb9ac473
Weisberg Barros <weisbergbsf@gmail.com>
2018-09-27 19:41:12 -0300

    App only with redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 import logo from '../../src/logo.svg';
 import '../../src/App.css';
+import { addRecipe } from '../actions';
 
 class App extends Component {
+  state = {
+    calendar: null
+  }
+  //Pegar a nossa store de props
+  componentDidMount() {
+    const { store } = this.props
+    // invocar o subscribe para qualquer mudança que acontecer dentro da nossa store do Redux
+    // e sempre que algo mudar, nós chamamos setState
+    store.subscribe(() => {
+      this.setState(() => ({
+        //pegar o stado da store e colocá-lo no estado do nosso componente local.
+        calendar: store.getState()
+      }))
+    })
+  }
+
+  submitFood = () => {
+    this.props.store.dispatch(addRecipe({
+      day: 'monday',
+      meal: 'breakfast',
+      recipe: {
+        label: this.input.value
+      }
+    }))
+    this.input.value = ''
+  }
+  
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input
+          type='text'
+          ref={(input) => this.input = input}
+          placeholder="Monday's Breakfast"
+        />
+        <button onClick={this.submitFood}>Submit</button>
+
+        <pre>
+          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
+        </pre>
       </div>
     );
   }
